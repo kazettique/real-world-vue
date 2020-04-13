66c19acac4924b490ab782bc25536d7ced0920da
Woody <woody.chang@cloud-interactive.com>
2020-04-13 18:18:43 +0800

    finished the course!

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService'
 
 export default {
   components: {
     EventCard
   },
+  data () {
+    return {
+      events: []
+    }
+  },
   created () {
-    axios
-      .get('http://localhost:3000/events')
-      .then(response => console.log(response.data))
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
       .catch(error => console.error(error))
   }
 }

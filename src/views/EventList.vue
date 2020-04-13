1fba430600b27fb1555564598671793756e0b4c8
Woody <woody.chang@cloud-interactive.com>
2020-04-13 16:45:15 +0800

    add json server

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,15 +2,23 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard />
+    <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
 export default {
   components: {
     EventCard
+  },
+  created () {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => console.log(response.data))
+      .catch(error => console.error(error))
   }
 }
 </script>

41a351edfe0d76c596f8e2596e5b2d6e05ece45a
Woody <woody.chang@cloud-interactive.com>
2020-04-16 20:42:41 +0800

    add base input component

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,31 +7,13 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field" />
+
+      <BaseInput label="Description" v-model="event.description" type="text" placeholder="Description" class="field" />
+
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput label="Location" v-model="event.location" type="text" placeholder="Location" class="field" />
+
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
@@ -72,15 +54,18 @@ export default {
   methods: {
     createEvent () {
       NProgress.start()
-      this.$store.dispatch('event/createEvent', this.event).then(() => {
-        this.$router.push({
-          name: 'event-show',
-          params: { id: this.event.id }
+      this.$store
+        .dispatch('event/createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEventObject()
         })
-        this.event = this.createFreshEventObject()
-      }).catch(() => {
-        NProgress.done()
-      })
+        .catch(() => {
+          NProgress.done()
+        })
     },
     createFreshEventObject () {
       const user = this.$store.state.user
